e5dff2eb364c2bfe16f385d8e9919ffa7542192a
Ittipat Pattum <nampu@finema.co>
2025-06-13 16:50:40 +0700

    save

--- ios/Classes/HelloPlugin.swift
+++ ios/Classes/HelloPlugin.swift
@@ -2,18 +2,24 @@ import Flutter
 import UIKit
 
 public class HelloPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "hello_plugin", binaryMessenger: registrar.messenger())
-    let instance = HelloPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(
+            name: "hello_plugin",
+            binaryMessenger: registrar.messenger()
+        )
+        let instance = HelloPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
